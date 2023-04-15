e99d91cc1ed2ef4466078122f14e45d82fb27b19
swyter <swyterzone@gmail.com>
2023-04-15 15:14:35 +0200

    First stubbed version.

---
+++ calcular-iban.py
@@ -0,0 +1,45 @@
+'''
+    https://en.wikipedia.org/wiki/International_Bank_Account_Number#cite_note-tr201-20
+    ESkk bbbb ssss xxcc cccc cccc
+    kk = Iban check digits code
+    b = National bank
+    s = Branch code
+    x = Check digits
+    c = Account number 
+'''
+
+iban = "ES04 2080 1795 2549 9916 ****" # ES0420801795254999165252
+ibstrip = iban.replace(" ", "").upper()
+ibstrip = ibstrip[4:] + ibstrip[:4]
+
+print(iban, ibstrip)
+
+ibarray = []
+
+for char in ibstrip:
+    print(char, char > 'A', )
+
+    try:
+        num = int(char)
+    except:
+        if ord(char) >= ord('A') and ord(char) <= ord('Z'):
+            num = 10 + (ord(char) - ord('A'))
+        else:
+            num = None
+
+    ibarray.append(num)
+
+ibnum = ""
+for i, elem in enumerate(ibarray):
+    print (i, elem)
+    if elem != None:
+        ibnum += str(elem)
+    else:
+        ibnum += 'X'
+
+print(ibarray, ibnum)
+
+for i in range(0000, 9999):
+    cur = int("2080179525499916%04u142804" % i)
+    if cur % 97 == 1:
+        print(f"[i] valid: {cur}")
