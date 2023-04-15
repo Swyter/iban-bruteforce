782e40a72c41639d46a80f50e21d2ff400d811f2
swyter <swyterzone@gmail.com>
2023-04-15 20:50:10 +0200

    Add support for bruteforcing the bank-branch block and adding it to the overall mix to reduce possibilities.

--- calcular-iban.py
+++ calcular-iban.py
@@ -8,7 +8,7 @@
     c = Account number 
 '''
 
-iban = "ES04 2080 *7** 2549 9916 5252" # ES0420801795254999165252
+iban = "ES04 *080 17*5 2549 9*16 525*" # ES0420801795254999165252
 ibstrip = iban.replace(" ", "").upper()
 
 es_bkbrnch_check_num = ibstrip[12]
@@ -27,13 +27,16 @@ print(f'''
     ESkk bbbb ssss xxcc cccc cccc
          \___ ___/ {ibstrip[12]}
                     {ibstrip[13]}\_ ____ ___/
+
+    \_{ibstrip[2:4]}________________________/
 '''
 )
 
 weights = [1, 2, 4, 8, 5, 10, 9, 7, 3, 6]
 
+bank_branch_code = "00" + ibstrip[4:12] # "20801795"
+
 if es_bkbrnch_check_num != '*':
-    bank_branch_code = "00" + ibstrip[4:12] # "20801795"
     try:
         int(bank_branch_code)
         total = 0
@@ -105,9 +108,9 @@ def fill_out_unknown(ibarray, generated_num = None):
     return ibnum
 
 print(ibarray, fill_out_unknown(ibarray))
-count_a = 0; count_b = 0; count_both = 0
+count_a = 0; count_b = 0; count_c = 0; count_both = 0
 for i in range(0000, 10 ** unknown_spaces):
-    valid_a = False; valid_b = False
+    valid_a = False; valid_b = False; valid_c = False
     cur_str = fill_out_unknown(ibarray, i) # ("2080179525499916%04u142804" % i)
     cur     = int(cur_str)
 
@@ -116,22 +119,37 @@ for i in range(0000, 10 ** unknown_spaces):
         count_a += 1
         valid_a = True
 
+    bank_branch_code = "00" + cur_str[0:8]
     account_num = cur_str[10:20]
-    total = 0
-    for j, elem in enumerate(account_num):
-        total += int(elem) * weights[j]
-    
-    check_num = 11 - (total % 11)
 
-    if es_account_can_be_checked and check_num == int(es_account_check_num):
-        #print(f"[-] [{i:04d}] valid Spanish check no: {account_num}")
-        count_b += 1
-        valid_b = True
+    if es_bkbrnch_can_be_checked:
+        total = 0
+        for j, elem in enumerate(bank_branch_code):
+            total += int(elem) * weights[j]
+        
+        check_num = 11 - (total % 11)
 
-    if valid_a and (valid_b or not es_bkbrnch_can_be_checked):
+        if check_num == int(es_bkbrnch_check_num):
+            #print(f"[-] [{i:04d}] valid Spanish check no: {account_num}")
+            count_c += 1
+            valid_c = True
+
+    if es_account_can_be_checked:
+        total = 0
+        for j, elem in enumerate(account_num):
+            total += int(elem) * weights[j]
+        
+        check_num = 11 - (total % 11)
+
+        if check_num == int(es_account_check_num):
+            #print(f"[-] [{i:04d}] valid Spanish check no: {account_num}")
+            count_b += 1
+            valid_b = True
+
+    if valid_a and (valid_b or not es_bkbrnch_can_be_checked) and (valid_c or not es_account_can_be_checked):
         print(f"[i] [{i:04d}] valid: {cur} % 97 == 1")
         print(f"[-] [{i:04d}] valid Spanish check no: {account_num}")
-        print(f"  \ both are valid for [{format_iban(ibstrip[-4:] + str(cur)[:20])}]")
+        print(f"  \ both are valid for the tentative IBAN number [{format_iban(ibstrip[-4:] + str(cur)[:20])}]")
         count_both += 1
 
-print(f" -- valid for a: {count_a}, valid for b: {count_b}, valid for both: {count_both}")
+print(f" -- valid for a: {count_a}, valid for b: {count_b}, valid for c: {count_c}, valid for both: {count_both}")
